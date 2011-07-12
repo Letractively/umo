6d3a89254ae9cca192fc89e246cd97c0353c17e4
jholgui@gmail.com <jholgui@gmail.com@797f5852-7413-db7a-cd56-1ef9e55983e0>
2011-07-12 13:11:44 +0000

    change type of someone var Add var maxtries

--- trunk/umoconfig.py
+++ trunk/umoconfig.py
@@ -1,11 +1,13 @@
-skippages = '0'
-pages = '10'
-results = '100'
-googlesleep = '5'
+skippages = 0
+pages = 10
+results = 100
+googlesleep = 5
 user_agent = 'code.google.com/umo'
 bingresults = 400
-bingkey = 'BING APP KEY'
+bingkey = 'Bing Key'
 depth = '1'
 malware = 'umomalware.log'
 safebrowsing = 'True'
 updatesafebrowsing = 'False'
+maxtries = 5
+
