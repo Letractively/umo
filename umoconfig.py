d8ebf1828ce4bada7b6d0fde300b51ec2354e238
jholgui <jholgui@gmail.com>
2011-07-18 22:28:23 +0000

    Add option urlsumo, where you will see URLs analyzed by UMO

--- umoconfig.py
+++ umoconfig.py
@@ -3,11 +3,12 @@ pages = 10
 results = 100
 googlesleep = 5
 user_agent = 'code.google.com/umo'
-bingresults = 400
-bingkey = 'Bing Key'
+bingresults = 999
+bingkey = ''
 depth = '1'
 malware = 'umomalware.log'
 safebrowsing = 'True'
 updatesafebrowsing = 'False'
 maxtries = 5
+urlsumo = 'urlsumo.log'
 
